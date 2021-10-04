74552a9822a4f57991a91a29904b57c88b4f2191
mintoko <84098196+mintoko@users.noreply.github.com>
2021-10-04 12:31:07 +0200

    Update test_fidl2adoc.py

--- fidl2adoc/test_fidl2adoc.py
+++ fidl2adoc/test_fidl2adoc.py
@@ -1,8 +1,12 @@
 from fidl2adoc import main
 import hashlib
-
+import os
 
 def test_output():
+    import os
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    for f in files:
+        print(f)  # do something
     main(['-i', '../examples/Test.fidl', '-o', '../examples/Test2.adoc'])
     hash1 = hashlib.md5(open('../examples/Test2.adoc','rb').read()).hexdigest()
     hash2 = hashlib.md5(open('../examples/Test.adoc','rb').read()).hexdigest()
