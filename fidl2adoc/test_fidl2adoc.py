12af8963e2609703661dc199887b8ee1fbd07b74
mintoko <heiko.schmidtevers@gmail.com>
2021-10-12 15:40:32 +0200

    Fix Lint

--- fidl2adoc/test_fidl2adoc.py
+++ fidl2adoc/test_fidl2adoc.py
@@ -1,12 +1,12 @@
 from fidl2adoc import main
 import hashlib
-import os
+
 
 def test_output():
     assert 0 == main(['-i', './examples/Test.fidl',
                      '-o', './examples/Test2.adoc'])
-    hash1 = hashlib.md5(open('./examples/Test2.adoc','rb').read()).hexdigest()
-    hash2 = hashlib.md5(open('./examples/Test.adoc','rb').read()).hexdigest()
+    hash1 = hashlib.md5(open('./examples/Test2.adoc', 'rb').read()).hexdigest()
+    hash2 = hashlib.md5(open('./examples/Test.adoc', 'rb').read()).hexdigest()
     assert(hash1 == hash2)
 
 
